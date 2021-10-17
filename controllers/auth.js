9896881ffbd58891235c0e570a9e73939c09babc
maseuko <mateuszgorczowski3@gmail.com>
2021-10-17 16:38:20 +0200

    Dodalem se zmienne es

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 
 exports.register = (req, res, next) => {
-
+    const test = "adad";
 }
 
 exports.login = (req, res, next) => {
--- routes/auth.js
+++ routes/auth.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+const asDAS = "";
 
 module.exports = router;
